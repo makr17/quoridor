f87c387d7b394ed9c248846df8380a216d12b011
mark bracher <mbracher@ziprecruiter.com>
2016-06-09 07:31:14 -0700

    continuing valid wall discovery

--- src/qplayer.erl
+++ src/qplayer.erl
@@ -16,8 +16,31 @@ valid_moves(B, Player) ->
     Pos = element(Player, B#board.positions),
     qutil:reachable_neighbors(B, Pos).
 
+% a1h .. h1h .... a8h .. h8h
+% a1v .. h1v .... a8v .. h8v
+% don't need to test Col i or Row 9 since walls have length of two
 valid_walls(B) ->
-    B.
+    valid_walls(B, lists:seq(1,8), ["a", "b", "c", "d", "e", "f", "g", "h"], []).
+
+valid_walls(_B, [], [], Acc) ->
+    Acc;
+valid_walls(B, [Row|Rows], Cols, Acc) ->
+    io:format("valid_walls: ~p~n", [Acc]),
+    HAcc = valid_row_walls(B, Row, Cols, "h", Acc),
+    VAcc = valid_row_walls(B, Row, Cols, "v", HAcc),
+    valid_walls(B, Rows, Cols, VAcc).
+
+valid_row_walls(_B, _, [], _, Acc) ->
+    Acc;
+valid_row_walls(B, Row, [Col|Cols], Orient, Acc) ->
+    Wall = Col ++ integer_to_list(Row) ++ Orient,
+    io:format("valid_row_walls: ~p~n", [Acc]),
+    case qutil:valid_wall(B, Wall) of
+	{valid, _} ->
+	    valid_row_walls(B, Row, Cols, Orient, [Wall|Acc]);
+	{invalid, _} ->
+	    valid_row_walls(B, Row, Cols, Orient, Acc)
+    end.
 
 score_board(B, Player) ->
     % TODO: stash in board if this becomes a burden
